eb9b27a25c3c4007e80c442d0e5ddf7a9bb68243
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-09-09 19:51:41 -0300

    Criação das tabelas: Paciente, Endereço e plano de saúde

---
+++ back-end/src/database/migrations/20200909190354-criatabelapaciente.js
@@ -0,0 +1,82 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    return queryInterface.createTable("tblPaciente", {
+
+      idPaciente: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      celular: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+
+      login: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      dataNascimento: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        unique: true
+      },
+
+      rg: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        unique: true
+      },
+
+      cpf: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        unique: true
+      },
+
+      foto: {
+        type: Sequelize.DATEONLY,
+        allowNull: true,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    return queryInterface.dropTable("tblPaciente");
+
+  }
+};
