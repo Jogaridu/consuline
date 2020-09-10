92414d89ef18011dc23d269fefa8322e40c131ef
Daniel Avila <leinadaavila01@gmail.com>
2020-09-10 13:18:48 -0300

    Criação das tabelas: Endereço, telefone, servico e profissional

---
+++ back-end/src/database/migrations/20200909211052-criatabelaprofissionaldasaude.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("tblProfissionalDaSaude", {
+      idProfissionalDaSaude: {
+        type: Sequelize.INTERGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      crm: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      login: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      foto: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      avaliacao: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdEnderecoProfissonalId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        refences: {
+          model: "tblEnderecoProfissional",
+          key: "idEnderecoProfissional",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("tblProfissionalDaSaude");
+  }
+};
---
+++ back-end/src/database/migrations/20200910154739-criatabelatblprofissionaltelefone.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("tblTelefoneProfissional", {
+      idTelefoneProfissional: {
+        type: Sequelize.INTERGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      numero: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdProfissionalDaSaudeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        refences: {
+          model: "tblProfissionalDaSaude",
+          key: "idProfissionalDaSaude",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("tblTelefoneProfissional");
+  }
+};
