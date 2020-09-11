bd17c30b11ecb7018daffde9fb5ef42b6b4a2238
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-09-11 15:31:20 -0300

    Script para cadastrar a central

--- back-end/src/database/migrations/20200909161755-criartabelacentral.js
+++ back-end/src/database/migrations/20200909161755-criartabelacentral.js
@@ -8,33 +8,48 @@ module.exports = {
         primaryKey: true,
         autoIncrement: true,
       },
+
       email: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: false,
       },
+
       login: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
       },
+
       senha: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       nome: {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
+
       updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
-    });
+
+    }).success(() => {
+      const queryInsertCentral = `insert into tblCentral 
+      (email, login, senha, nome, created_at, updated_at) values 
+      ('centralConsuline@gmail.com', 'admin', 'admin', 'Central Admistradora', NOW(), NOW());`;
+
+      migration.migrator.sequelize.query(queryInsertCentral);
+
+      done();
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
