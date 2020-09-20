ecf8bb49e89e536281a781a03f48e8834de388f4
Nicolas Santos <ns5077900@gmail.com>
2020-09-20 17:45:34 -0300

    Teste

--- front-end/src/Pages/Login/index.js
+++ front-end/src/Pages/Login/index.js
@@ -1,44 +1,41 @@
-import React from 'react';
+import React from "react";
 
 import "./styles.css";
 
-
-
 import "../../Styles/globalStyle.css";
 
-import logoprojeto2 from "../../Assets/logoprojeto2.png"
+import logoprojeto2 from "../../Assets/logoprojeto2.png";
 
 const Login = () => {
-
   return (
     <>
-        <body>
-            <div className="container-principal">
-                <div className="container-imagem">
-                    <img src={logoprojeto2} alt="logo projeto" />
-                </div>
-                <div className="box">
-                        <form>
-                            <div>
-                              <input type="text" name="" required=""></input>
-                                <label>Login</label>
-                            </div>
-
-                            <div>
-                              <input type="password" name="" required=""></input>
-                                <label>Senha</label>
-                            </div>
-
-                            <div className="conteiner-botoes">
-                              <button type="button"> Voltar</button>
-                              <button type="button" > Iniciar sessão</button>
-                            </div>
-                        </form>
+      <body>
+        <div className="container-principal">
+          <div className="container-imagem">
+            <img src={logoprojeto2} alt="logo projeto" />
+          </div>
+          <div className="box">
+            <form>
+              <div>
+                <input type="text" name="" required=""></input>
+                <label>Login</label>
               </div>
-            </div>
-        </body>
+
+              <div>
+                <input type="password" name="" required=""></input>
+                <label>Senha</label>
+              </div>
+
+              <div className="conteiner-botoes">
+                <button type="button"> Voltar</button>
+                <button type="button"> Iniciar sessão</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </body>
     </>
   );
-}
+};
 
 export default Login;
