08018921e86c90f2f859277f8d4d5425be84940a
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-09-20 17:25:14 -0300

    Ajuste página login: Alinhamentos

--- front-end/src/Pages/Login/index.js
+++ front-end/src/Pages/Login/index.js
@@ -12,31 +12,35 @@ const Login = () => {
 
   return (
     <>
-        <body>
-            <div className="container-principal">
-                <div className="container-imagem">
-                    <img src={logoprojeto2} alt="logo projeto" />
-                </div>
-                <div className="box">
-                        <form>
-                            <div>
-                              <input type="text" name="" required=""></input>
-                                <label>Login</label>
-                            </div>
-
-                            <div>
-                              <input type="password" name="" required=""></input>
-                                <label>Senha</label>
-                            </div>
-
-                            <div className="conteiner-botoes">
-                              <button type="button"> Voltar</button>
-                              <button type="button" > Iniciar sessão</button>
-                            </div>
-                        </form>
-              </div>
+      <body>
+        <div className="container">
+          <div className="container-principal">
+            <div className="container-imagem">
+              <img src={logoprojeto2} alt="logo projeto" />
             </div>
-        </body>
+            <div className="box">
+
+              <form>
+                <div>
+                  <input type="text" name="" required=""></input>
+                  <label>Login</label>
+                </div>
+
+                <div>
+                  <input type="password" name="" required=""></input>
+                  <label>Senha</label>
+                </div>
+
+                <div className="conteiner-botoes">
+                  <button type="button"> Voltar</button>
+                  <button type="button" > Iniciar sessão</button>
+                </div>
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </body>
     </>
   );
 }
