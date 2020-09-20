17bbe8d0d58f3313724273cad70589a780e81ef0
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-09-19 23:30:54 -0300

    CRUD Serviços

--- back-end/src/Routes/Private/servicos.js
+++ back-end/src/Routes/Private/servicos.js
@@ -4,6 +4,14 @@ const routes = express.Router();
 
 const controllerServico = require("../../controllers/servicos");
 
-routes.post("/servicos", controllerServico.store);
+routes.post("/servicos", controllerServico.cadastrar);
+
+routes.get("/servico/:id", controllerServico.buscarPorId);
+
+routes.get("/servicos", controllerServico.listar);
+
+routes.delete("/servico/:id", controllerServico.deletar);
+
+routes.put("/servico/:id", controllerServico.atualizar);
 
 module.exports = routes;
