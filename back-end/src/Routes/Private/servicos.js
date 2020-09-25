cfbf3609f27d6c6cd7e34aa99b1e58f867b5b122
Vinicius Reis <guaguinhireis@gmail.com>
2020-09-25 16:37:06 -0300

    Criando update de paciete

--- back-end/src/Routes/Private/servicos.js
+++ back-end/src/Routes/Private/servicos.js
@@ -7,7 +7,7 @@ const controllerServico = require("../../controllers/servicos");
 const Multer = require("../../fixtures/manipulacaoForm");
 const enviarImagem = require("../../services/firebase");
 
-routes.post("/servicos", Multer("foto"), enviarImagem, controllerServico.cadastrar);
+// routes.post("/servicos", Multer("foto"), enviarImagem, controllerServico.cadastrar);
 
 routes.get("/servico/:id", controllerServico.buscarPorId);
 
