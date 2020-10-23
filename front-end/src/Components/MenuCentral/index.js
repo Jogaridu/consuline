65bbc28f349a98b48b11a5248de1b7d506ec4e39
Jorge Gabriel <e-jorge2010@hotmail.com>
2020-10-22 23:38:14 -0300

    Rota para home-central, no menu

--- front-end/src/Components/MenuCentral/index.js
+++ front-end/src/Components/MenuCentral/index.js
@@ -8,10 +8,11 @@ import { Link } from 'react-router-dom';
 function MenuCentral() {
     return (
         <div className="menu">
-            <figure className="iconeMenu">
-                <img id="logoprojeto" src={logoConsuline} alt="Logo da empresa Consuline" />
-            </figure>
-
+            <Link to="/home-central">
+                <figure className="iconeMenu">
+                    <img id="logoprojeto" src={logoConsuline} alt="Logo da empresa Consuline" />
+                </figure>
+            </Link>
             <div className="topicosMenu">
 
                 <div className="itensMenu">
@@ -33,7 +34,7 @@ function MenuCentral() {
             <div className="desenvolvedor">
                 Desenvolvido por <br /> DS3-M | CONSULINE
             </div>
-        </div>
+        </div >
     );
 }
 
