c843c6678fc7195588b58397e55706f402715784
brunogm2 <brunobgm002@gmail.com>
2020-11-06 13:44:13 -0300

    Tela de consulta pronta (HTML | CSS)

--- front-end/src/Pages/trabalhoMedico/home/index.js
+++ front-end/src/Pages/trabalhoMedico/home/index.js
@@ -3,16 +3,24 @@ import React from 'react';
 import './style.css';
 import '../../../Styles/globalStyle.css'
 import supermedico from '../../../Assets/supermedico.png'
-// import medicoteste from '../../../Assets/medicoteste.png'
+import medicoteste from '../../../Assets/medicoteste.png'
 
 function HomeConsulta(){
     return(
         <div className="container-consulta-home">
            <div id="bem-vindo">
-                <div className="img-supermedico">
+                <div 
+                data-aos="fade-right"
+                data-aos-delay="80"
+                data-aos-duration="900"
+                className="img-supermedico">
                     <img id="supermedico" src={supermedico} alt="Imagem de teste"/>
                 </div>
-                <div className="subtext-bemvindo">
+                <div 
+                data-aos="fade-right"
+                data-aos-delay="80"
+                data-aos-duration="900"
+                className="subtext-bemvindo">
                     <h1>Olá,</h1>
                     <h5>Lembre-se sempre do seu juramento:</h5> 
                     <p>“Eu prometo solenemente consagrar minha 
@@ -27,18 +35,110 @@ function HomeConsulta(){
                 </div>
                 <div className="card-previa-consulta">
                     <div className="card-previa">
-                        
+                        <div className="foto-previa-card">
+                        <img id="medicoteste" src={medicoteste} alt="Logoteste" /> 
+                        </div>
+                        <div className="nome-previa-card">
+                            Bruno Gonçalves
+                        </div>      
+                        <div className="data-previa-card">
+                            06/11, 15:00.
+                        </div>
+                        <div className="vermais-previa-card">
+                            <div className="txtvermais-previa-card">
+                                Ver mais
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-previa">
+                        <div className="foto-previa-card">
+                        <img id="medicoteste" src={medicoteste} alt="Logoteste" /> 
+                        </div>
+                        <div className="nome-previa-card">
+                            Bruno Gonçalves
+                        </div>      
+                        <div className="data-previa-card">
+                            06/11, 15:00.
+                        </div>
+                        <div className="vermais-previa-card">
+                            <div className="txtvermais-previa-card">
+                                Ver mais
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-previa">
+                        <div className="foto-previa-card">
+                        <img id="medicoteste" src={medicoteste} alt="Logoteste" /> 
+                        </div>
+                        <div className="nome-previa-card">
+                            Bruno Gonçalves
+                        </div>      
+                        <div className="data-previa-card">
+                            06/11, 15:00.
+                        </div>
+                        <div className="vermais-previa-card">
+                            <div className="txtvermais-previa-card">
+                                Ver mais
+                            </div>
+                        </div>
                     </div>
                 </div>       
-                <div className="mais-consulta">
-
+            <div className="mais-consulta">
+                    Mais Consultas +
                 </div>
            </div>
            <div id="informacoes-consultas">
-
+                <div className="previa-perfil-medico">
+                    <div className="img-perfil-medico-previa">
+                     <img id="medicoteste" src={medicoteste} alt="Logoteste" /> 
+                    </div>
+                    <div className="nome-perfil-medico-previa">
+                        Bruno Gonçalves
+                    </div>
+                    <div className="cidade-perfil-medico-previa">
+                        Osasco, SP
+                    </div>
+                </div>
+                <div className="informacoes-agendamento-medico">
+                    <div className="txtprevia-perfil-medico">
+                        Agendamento
+                    </div>
+                    <div className="txtcontagemprevia-perfil-medico">
+                        50
+                    </div>
+                </div>
+                <div className="informacoes-totalconsulta-medico">
+                    <div className="txtprevia-perfil-medico">
+                        Total consultas
+                    </div>
+                    <div className="txtcontagemprevia-perfil-medico">
+                        150
+                    </div>
+                </div>
            </div>
            <div id="apresentacao-avaliacao">
-
+               <div className="titulo-card-avaliacoes">
+                    Avaliações
+               </div>
+               <div className="mini-card-avaliacao">
+                    <div className="card-ratingbar">
+                        <div className="img-cliente-avaliador">
+                            <div className="caixa-ajuste-medico">
+                            <img id="medicoteste1" src={medicoteste} alt="" /> 
+                            </div>
+                        </div>
+                        <div className="nome-cliente-avaliador">
+                            Bruno Gonçalves
+                        </div>
+                        <div className="ratingbar-cliente-avaliador">
+                            
+                        </div>
+                    </div>
+               </div>
+               
+               <div className="vermais-card-avaliacoes">
+                    Ver mais
+               </div>
            </div>
         </div>
     );
