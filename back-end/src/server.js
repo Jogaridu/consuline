301ac97b7ee788e91b3bdf78fb6877e41e094c03
ViniciusROReis07 <guaguinhireis@gmail.com>
2020-12-07 10:07:55 -0300

    privando arquivos

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-  console.log("Servidor rodando na porta" + PORT);
+  console.log("Servidor rodando na porta " + PORT);
 });
